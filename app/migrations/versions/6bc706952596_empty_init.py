ef4b86435b476692517fd305d52b5d2b9e40fecf
Moreen <moreencharles@gmail.com>
2024-01-11 19:18:15 +0300

    Completed Assignment

---
+++ app/migrations/versions/6bc706952596_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 6bc706952596
+Revises: 
+Create Date: 2024-01-11 19:07:35.694525
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6bc706952596'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
